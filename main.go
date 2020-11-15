ee21d5d29ccda36ce8274e29a2bfdd8a695a487a
Nator Verinumbe <natverior1@gmail.com>
2020-11-15 16:20:20 +0100

    added userConstroller OOP structure for mongodb

--- main.go
+++ main.go
@@ -16,9 +16,10 @@ import (
 
 func main() {
 
+	uc := controllers.NewUserController(controllers.GetSession()) //Creates a Mongo Db session
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/api/insert", controllers.InsertHandler).Methods("POST")
-	router.HandleFunc("/api/fetch/{id}", controllers.FetchHandler).Methods("GET")
+	router.HandleFunc("/api/insert", uc.InsertHandler).Methods("POST")
+	router.HandleFunc("/api/fetch/{id}", uc.FetchHandler).Methods("GET")
 
 	fmt.Println("Server started on part 9080")
 	log.Fatal(http.ListenAndServe(":9080", router))
